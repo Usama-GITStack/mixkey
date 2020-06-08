cbed853998f42e162fbe070ee23d56bf12e48120
omar saleem <omar.skylinxtech@gmail.com>
2020-06-08 17:20:29 +0500

    check forget password

--- helper/utils.js
+++ helper/utils.js
@@ -116,7 +116,7 @@ utils.sendEmail = (toEmail, subject, body, callback) => {
     }
     // createTransport({ host: "smtp.gmail.com", port: 587, auth: { user: "username@gmail.com", pass: "app password" } });
     let transporter = nodemailer.createTransport({
-        service: 'multikey.app',
+        host: 'multikey.app',
         // service: 'gmail',
         port: 25,
         auth: {
