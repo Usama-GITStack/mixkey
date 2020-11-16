929bc6d39f54da9dc4eae996dba31ea69cce9cb6
Faizan Chughtai <faizanchughtai@Faizans-MacBook-Pro.local>
2020-11-16 17:49:29 +0500

    db change

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 let mongoose = require('mongoose');
-mongoose.connect('mongodb://heroku_skylinxtech:ayKXVmyd8ZdNeb@ds053529.mlab.com:53529/heroku_r9hh0p1k');
+// mongoose.connect('mongodb://heroku_skylinxtech:ayKXVmyd8ZdNeb@ds053529.mlab.com:53529/heroku_r9hh0p1k');
+mongoose.connect('mongodb+srv://heroku_skylinxtech:tYE.frkN9EjVfbVr@cluster-r9hh0p1k.tcnns.mongodb.net/heroku_r9hh0p1k');
 
 mongoose.connection.on('error', function(err) {
     console.log(err);
